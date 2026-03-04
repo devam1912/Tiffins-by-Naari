7492a8f490645f72ec879f2fc7117e82c7d900ae
kajalVarlani <kajalvarlani937@gmail.com>
2026-03-04 14:40:47 +0530

    Resolved merge conflicts

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import CustomerDashboard from "./pages/CustomerDashboard";
-import CustomerProfile from "./pages/CustomerProfile";
+import CustomerDashboard from "./pages/Customer/CustomerDashboard";
+import CustomerProfile from "./pages/Customer/CustomerProfile";
 import VerifyOtp from "./pages/VerifyOtp";
 import { ProviderDashboard } from "./pages/provider/ProviderDashboard";
 export default function App() {
